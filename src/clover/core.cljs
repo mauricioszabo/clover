179a6df23d265256d3c579e40077761c7d4f1ce9
Maurício Szabo <mauricio.szabo@gmail.com>
2019-09-08 21:50:15 -0300

    Creating webview, experimenting with goto-var-definition

--- src/clover/core.cljs
+++ src/clover/core.cljs
@@ -1,13 +1,41 @@
 (ns clover.core
   (:require [clover.aux :as aux :include-macros true]
             [clover.vs :as vs]
+            [clover.state :as st]
+            [clover.ui :as ui]
             [clover.commands.connection :as conn]
-            ["vscode" :as vscode]))
+            [repl-tooling.features.definition :as definition]
+            [repl-tooling.editor-helpers :as helpers]
+            ["vscode" :as vscode :refer [Location Uri Position]]
+            ["path" :as path]))
 
-(defn activate [_ctx]
+(defn- var-definition [document position]
+  (let [data (vs/get-document-data document position)
+        curr-var (helpers/current-var (:contents data) (-> data :range first))
+        [_ curr-ns] (helpers/ns-range-for (:contents data) (-> data :range first))]
+    (.. (definition/find-var-definition (st/repl-for-aux) curr-ns curr-var)
+        (then (fn [{:keys [file-name line]}]
+                (Location. (. Uri parse file-name) (Position. line 0)))))))
+
+(defn activate [^js ctx]
+  (when ctx
+    (reset! ui/curr-dir (.. ctx -extensionPath))
+    (let [res-path (.. vscode -Uri (file (. path join @ui/curr-dir "view")))]
+      (reset! ui/view (.. vscode -window
+                          (createWebviewPanel "chlorine-console"
+                                              "Chlorine REPL"
+                                              (.. vscode -ViewColumn -Two)
+                                              #js {:enableScripts true
+                                                   :localResourceRoots #js [res-path]}))))
+    (ui/render-console!))
+
   (aux/add-disposable! (.. vscode -commands
                            (registerCommand "extension.connectSocketRepl"
-                                            conn/connect!))))
+                                            conn/connect!)))
+  (aux/add-disposable! (.. vscode -languages
+                           (registerDefinitionProvider
+                            "clojure"
+                            #js {:provideDefinition var-definition}))))
 
 (defn deactivate []
   (aux/clear-all!))
