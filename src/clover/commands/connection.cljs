383237b4de18cc8c7bddc09c4e3264b55ba3e9cb
Maurício Szabo <mauricio.szabo@gmail.com>
2020-11-02 16:41:01 -0300

    Adding custom tasks

--- src/clover/commands/connection.cljs
+++ src/clover/commands/connection.cljs
@@ -1,13 +1,55 @@
 (ns clover.commands.connection
   (:require [repl-tooling.editor-integration.connection :as connection]
+            [repl-tooling.editor-integration.configs :as configs]
             [clover.vs :as vs]
             [clover.aux :as aux :include-macros true]
             [clover.ui :as ui]
             [clover.state :as state]
             [clojure.string :as str]
-            ["vscode" :as vscode]
+            [promesa.core :as p]
+            ["vscode" :as vscode :refer [Task TaskDefinition]]
             ["path" :as path]
-            ["fs" :refer [existsSync readFileSync]]))
+            ["os" :as os]
+            ["fs" :refer [existsSync readFileSync] :as fs]))
+
+(defn- create-pseudo-terminal [command]
+  (let [emitter (-> vscode .-EventEmitter new)]
+    (new (. vscode -CustomExecution)
+      (fn [_]
+        #js {:onDidClose (.-event emitter)
+             :onDidWrite prn
+             :close prn
+             :handleInput prn
+             :open (fn [& _]
+                     (command)
+                     (.fire ^js emitter))
+             :setDimensions prn}))))
+
+(defn- create-task [command-key command-name function]
+  (doto (new Task
+          #js {:type "clover" :command (name command-key)}
+          (.. vscode -TaskScope -Workspace)
+          command-name
+          "clover"
+          (create-pseudo-terminal function))
+        (aset "presentationOptions"
+              #js {:reveal (.. vscode -TaskRevealKind -Never)
+                   :clear true})))
+
+(defonce custom-commands (atom {}))
+
+(def provider
+  #js {:provideTasks (fn [_]
+                       (->> @custom-commands
+                            (map (fn [[k v]]
+                                   (create-task k (:name v) (:command v))))
+                            clj->js))
+       :resolveTask (fn [_ _])})
+
+(aux/add-disposable!
+  (.. vscode -tasks
+      (registerTaskProvider "clover"
+                            provider)))
 
 (defn- extract-host-port [txt]
   (let [[host port] (str/split txt #":")
@@ -64,33 +106,59 @@
                               (old-cmd)
                               (new-cmd))))))))
 
+(def ^:private original-cmds
+  #{"evaluate-block"
+    "evaluate-top-block"
+    "evaluate-selection"
+    "clear-console"
+    "disconnect"
+    "load-file"
+    "break-evaluation"
+    "doc-for-var"
+    "run-tests-in-ns"
+    "run-test-for-var"
+    "source-for-var"
+    "connect-embedded"
+    "open-config"
+    "goto-var-definition"})
+
 (defn- register-commands! [commands]
   (let [experimental? (.. vscode -workspace (getConfiguration "clover") -experimental)]
     (aux/clear-commands!)
+    (reset! custom-commands {})
+
     (doseq [[k command] commands]
-      (decide-command k command experimental?))))
+      (if (original-cmds (name k))
+        (decide-command k command experimental?)
+        (swap! custom-commands assoc k command)))))
+
+(defn- open-editor [{:keys [file-name line column]}]
+  (p/let [doc (.. vscode -workspace (openTextDocument file-name))]
+    (.. vscode -window (showTextDocument doc))))
 
 (defn- connect-clj [[host port]]
-  (.. (connection/connect!
-       host port
-       {:on-stdout #(ui/send-output! :stdout %)
-        :on-stderr #(ui/send-output! :stderr %)
-        :on-disconnect disconnect!
-        :prompt vs/choice
-        :get-config get-config
-        :on-eval #(ui/send-result! % :clj)
-        :on-patch #(ui/post-message! {:command :patch :obj %})
-        :editor-data vs/get-editor-data
-        :register-commands register-commands!
-        :notify notify!})
-      (then #(when-let [st %]
-               (swap! state/state assoc :conn st)
-               ; (doseq [[key {:keys [command]}] (-> @st :editor/commands)]
-               ;   (aux/add-transient! (.. vscode
-               ;                           -commands
-               ;                           (registerCommand (str "clover." (name key))
-               ;                                            command))))
-               (register-console!)))))
+  (let [config-file (path/join (.homedir os) ".config" "clover" "config.cljs")]
+    (when-not (fs/existsSync config-file)
+      (try (fs/mkdirSync (path/dirname config-file)) (catch :default _))
+      (fs/closeSync (fs/openSync config-file "w")))
+
+    (.. (connection/connect!
+         host port
+         {:on-stdout #(ui/send-output! :stdout %)
+          :on-stderr #(ui/send-output! :stderr %)
+          :on-disconnect disconnect!
+          :prompt vs/choice
+          :get-config get-config
+          :open-editor open-editor
+          :config-file-path config-file
+          :on-eval #(ui/send-result! % :clj)
+          :on-patch #(ui/post-message! {:command :patch :obj %})
+          :editor-data vs/get-editor-data
+          :register-commands register-commands!
+          :notify notify!})
+        (then #(when-let [st %]
+                 (swap! state/state assoc :conn st)
+                 (register-console!))))))
 
 (defn- find-shadow-port []
   (->> (folders)
